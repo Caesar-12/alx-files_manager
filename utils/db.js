2929925d10bb50b14c16a3cc792c56c7f2604137
Caesar-12 <idowuenoch65@gmail.com>
2023-12-04 15:23:04 +0000

    MongoDB client

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.URI = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(this.URI, { useUnifiedTopology: true });
+
+    this.client.connect()
+      .then(() => {
+        console.log('Connected to MongoDB!');
+      })
+      .catch((error) => {
+        console.error('MongoDB connection error:', error);
+      });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const users = this.client.db().collection('users');
+    return users.countDocuments();
+  }
+
+  async nbFiles() {
+    const users = this.client.db().collection('files');
+    return users.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
