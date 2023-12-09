cfca6a2b699409b7ab740947d5b53a7194fbdeac
Caesar-12 <idowuenoch65@gmail.com>
2023-12-09 03:23:19 +0000

    simple API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,26 +1,20 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const AppController = {
-  getStatus: async (req, res) => {
-    try {
-      redisStatus = redisClient.isAlive();
-      dbStatus = dbClient.isAlive();
-      res.status(200).json({ redis: redisStatus, db: dbStatus });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.json({ redis: true, db: true });
+      res.end();
     }
-  },
-  getStats: async (req, res) => {
-    try {
-        const uesrCount = dbClient.nbUsers();
-        const fileCount = dbClient.nbFiles();
+  }
 
-        res.status(200).json({ users: uesrCount, files: fileCount });
-    } catch (error) {
-        res.status(500).json({ error: error});
-    }
-  },
-};
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.json({ users, files });
+    res.end();
+  }
+}
 
 module.exports = AppController;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
 
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/', routes);
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
